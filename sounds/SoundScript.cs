7196ecda4363c90cb5b43e020f05c2c39234c3e4
Nathan Beaudoin <115648764+nath-bea@users.noreply.github.com>
2024-02-14 23:27:41 -0500

    Add files via upload

---
+++ sounds/SoundScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+
+    public AudioSource soundplay;
+
+
+
+    public void play2()
+    {
+
+        
+            soundplay.Play();
+        
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
