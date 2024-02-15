b0d6c31d7918ec88ea3ad7e99ff3f02de3f562d4
Nathan Beaudoin <115648764+nath-bea@users.noreply.github.com>
2024-02-14 23:19:29 -0500

    Add files via upload

---
+++ controller code/bounceSound.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounceSound : MonoBehaviour
+{
+    public AudioSource bounce;
+
+    public void play3()
+    {
+        bounce.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
