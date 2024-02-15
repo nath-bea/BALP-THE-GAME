819147fcc721b7e035a39f0ca04b00447f756183
Nathan Beaudoin <115648764+nath-bea@users.noreply.github.com>
2024-02-14 23:23:48 -0500

    Add files via upload

---
+++ controller code/Selector.cs
@@ -0,0 +1,212 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Selector : MonoBehaviour
+{
+
+    // holds all the subroutines for the selection menu
+    public void poop()
+    {
+        
+        SceneManager.LoadScene("Select");
+    }
+    public void one()
+    {
+        resourceManager.level = 1;
+        SceneManager.LoadScene("Scene 1");
+    }
+    public void two()
+    {
+        if (resourceManager.lvl2 == true)
+        {
+            resourceManager.level = 2;
+            SceneManager.LoadScene("Scene 2");
+        }
+        
+    }
+    public void three()
+    {
+        if (resourceManager.lvl3 == true)
+        {
+            resourceManager.level = 3;
+            SceneManager.LoadScene("Scene 3");
+        }
+        
+    }
+    public void four()
+    {
+        if (resourceManager.lvl4 == true)
+        {
+            resourceManager.level = 4;
+            SceneManager.LoadScene("Scene 4");
+        }
+        
+    }
+    public void five()
+    {
+        if (resourceManager.lvl5 == true)
+        {
+            resourceManager.level = 5;
+            SceneManager.LoadScene("Scene 5");
+        }
+        
+    }
+    public void six()
+    {
+        if (resourceManager.lvl6 == true)
+        {
+            resourceManager.level = 6;
+            SceneManager.LoadScene("Scene 6");
+        }
+        
+    }
+    public void seven()
+    {
+        if (resourceManager.lvl7 == true)
+        {
+            resourceManager.level = 7;
+            SceneManager.LoadScene("Scene 7");
+        }
+        
+    }
+    public void eight()
+    {
+        if (resourceManager.lvl8 == true)
+        {
+            resourceManager.level = 8;
+            SceneManager.LoadScene("Scene 8");
+        }
+        
+    }
+    public void nine()
+    {
+        if (resourceManager.lvl9 == true)
+        {
+            resourceManager.level = 9;
+            SceneManager.LoadScene("Scene 9");
+        }
+        
+    }
+    public void ten()
+    {
+        if (resourceManager.lvl10 == true)
+        {
+            resourceManager.level = 10;
+            SceneManager.LoadScene("Scene 10");
+        }
+        
+    }
+    public void eleven()
+    {
+        if (resourceManager.lvl11 == true)
+        {
+            resourceManager.level = 11;
+            SceneManager.LoadScene("Scene 11");
+        }
+        
+    }
+    public void twelve()
+    {
+        if (resourceManager.lvl12 == true)
+        {
+            resourceManager.level = 12;
+            SceneManager.LoadScene("Scene 12");
+        }
+        
+    }
+    public void thirteen()
+    {
+        if (resourceManager.lvl13 == true)
+        {
+            resourceManager.level = 13;
+            SceneManager.LoadScene("Scene 13");
+        }
+        
+    }
+    public void fourteen()
+    {
+        if (resourceManager.lvl14 == true)
+        {
+            resourceManager.level = 14;
+            SceneManager.LoadScene("Scene 14");
+        }
+        
+    }
+    public void fifteen()
+    {
+        if (resourceManager.lvl15 == true)
+        {
+            resourceManager.level = 15;
+            SceneManager.LoadScene("Scene 15");
+        }
+        
+    }
+    public void sixteen()
+    {
+        if (resourceManager.lvl16 == true)
+        {
+            resourceManager.level = 16;
+            SceneManager.LoadScene("Scene 16");
+        }
+        
+    }
+    public void seventeen()
+    {
+        if (resourceManager.lvl17 == true)
+        {
+            resourceManager.level = 17;
+            SceneManager.LoadScene("Scene 17");
+        }
+        
+    }
+    public void eighteen()
+    {
+        if (resourceManager.lvl18 == true)
+        {
+            resourceManager.level = 18;
+            SceneManager.LoadScene("Scene 18");
+        }
+        
+    }
+    public void nineteen()
+    {
+        if (resourceManager.lvl19 == true)
+        {
+            resourceManager.level = 19;
+            SceneManager.LoadScene("Scene 19");
+        }
+        
+    }
+    public void twenty()
+    {
+        if (resourceManager.lvl20 == true)
+        {
+            resourceManager.level = 20;
+            SceneManager.LoadScene("Scene 20");
+        }
+        
+    }
+    public void twentyone()
+    {
+        if (resourceManager.lvl21 == true)
+        {
+            resourceManager.level = 21;
+            SceneManager.LoadScene("Scene 21");
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
